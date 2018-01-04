f8a9169ce3502e2af70bb8819687c37ddb948fe7
khalilliu <lkhaliliu@gmail.com>
2018-01-04 22:04:06 +0800

    add browserify

--- module1/gulpfile.js
+++ module1/gulpfile.js
@@ -3,13 +3,18 @@
 var gulp  = require('gulp');
 var connect = require('gulp-connect'); //run a local dev server
 var open = require('gulp-open'); //open a url in browser
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 var config = {
 	post: 8080,
 	devBaseUrl: 'http://localhost',
 	paths:{
 		html: './src/*.html',
-		dist: './dist'
+		js: './src/**/*.js',
+		dist: './dist',
+		mainJs: './src/main.js'
 	}
 }
 
@@ -36,9 +41,20 @@ gulp.task('html',function(){
 		.pipe(connect.reload());
 })
 
+gulp.task('js',function(){
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error',console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+})
+
 //watch the file change
 gulp.task('watch',function(){
 	gulp.watch(config.paths.html,['html'])
+	gulp.watch(config.paths.js,['js'])
 })
 
-gulp.task('default',['html','open','watch']);
+gulp.task('default',['html','js','open','watch']);
